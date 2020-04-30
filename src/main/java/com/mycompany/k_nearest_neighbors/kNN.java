795fa69fd9245e430c71f97a9b7515064ee765ce
grand <grand@DESKTOP-J7RCQHK>
2020-04-29 23:16:02 -0500

    kNN project

--- src/main/java/com/mycompany/k_nearest_neighbors/kNN.java
+++ src/main/java/com/mycompany/k_nearest_neighbors/kNN.java
@@ -1,5 +1,6 @@
 package com.mycompany.k_nearest_neighbors;
 
+import static com.mycompany.k_nearest_neighbors.kNN.neighbors;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.io.IOException;
@@ -7,22 +8,46 @@ import java.io.IOException;
 
 
 public class kNN {
-
-
-    public static void main(String[] args) throws IOException {
-        
+  
+    public static void main(String[] args) throws IOException, InterruptedException {
+        long startTime = System.nanoTime();
         int[][] trainingImages = mnist.mnistImageReader("data/train-images.idx3-ubyte");
         int[] trainingLabels = mnist.mnistLabelReader("data/train-labels.idx1-ubyte");
         
         int[][] testImages = mnist.mnistImageReader("data/t10k-images.idx3-ubyte");
         int[] testLabels = mnist.mnistLabelReader("data/t10k-labels.idx1-ubyte");
-        // our programs guess based on the knn implementaion
+        double accuracy=0;
+
+        int k=9;
+       
+       
         int[] guess = new int[testImages.length];
-        // array to hold the distance of all the traininng points from the test point
-        double[][] distance= new double[trainingImages.length][2];
-        int k=245;
-        double dist;
         
+        distance a = new distance(guess, trainingImages, testImages, trainingLabels, k, 0, 2500);
+        distance b = new distance(guess, trainingImages, testImages, trainingLabels, k, 2500, 5000);
+        distance c = new distance(guess, trainingImages, testImages, trainingLabels, k, 5000, 7500);
+        distance d = new distance(guess, trainingImages, testImages, trainingLabels, k, 7500, 10000);
+        
+        Thread t1= new Thread(a);
+        Thread t2= new Thread(b);
+        Thread t3= new Thread(c);
+        Thread t4= new Thread(d);
+        
+        t1.start();
+        t1.join();
+        
+        t2.start();
+        t2.join();
+        t3.start();
+        t3.join();
+        t4.start();
+        t4.join();
+       
+        /*
+        double[][] distance= new double[trainingImages.length][2];
+        
+        double dist;
+       
         for(int i=0; i<testImages.length;i++){
             for(int j=0; j<trainingImages.length; j++){
                 dist=0;
@@ -37,12 +62,14 @@ public class kNN {
             guess[i]=neighbors(distance,k);
         }
        //see how accurate the knn is
-        double accuracy=0;
-        for(int i = 0; i<testLabels.length;i++) if(guess[i]==testLabels[i]) accuracy=accuracy+1;
+        */
+        for(int i = 0; i<testLabels.length;i++) if(guess[i]==testLabels[i]) accuracy=accuracy+1; 
         
         accuracy=accuracy/testLabels.length*100;
         System.out.print(accuracy);
-  
+       long endTime = System.nanoTime();
+       System.out.println("Took "+(endTime - startTime) + " ns"); 
+      
     }
     
     // since the digits are 0-9 set up the indexs to match the digits
@@ -64,8 +91,45 @@ public class kNN {
         
         if(a[right]>a[left])return right;
         else return left;
+    } 
+}
+
+ class distance implements Runnable{
+    double[][] distArr = new double[60000][2];
+    int[] guess;
+    int[][] mnistTrainArray;
+    int[][] mnistTestArray;
+    int[] mnistTrainLabels;
+    int knn;
+    int begin;
+    int end;
+    public distance(int[] guess,int[][] mnistTrainArray, int[][] mnistTestArray, int[] mnistTrainLabels,int k, int begin, int end){
+         this.guess=guess;
+         this.mnistTrainArray=mnistTrainArray;
+         this.mnistTestArray=mnistTestArray;
+         this.mnistTrainLabels=mnistTrainLabels;
+         this.knn=k;
+         this.begin=begin;
+         this.end=end;
     }
     
-
-    
-}
+    @Override
+    public void run(){
+       
+       
+        for(int i=begin; i<end;++i){
+            for(int j=0; j<mnistTrainArray.length; ++j){
+                dist=0;
+                for(int k=0;k<mnistTrainArray[j].length;++k){
+                    distArr[j][0]=distArr[j][0]+Math.pow((mnistTrainArray[j][k]-mnistTestArray[i][k]),2);
+                }
+                distArr[j][1]=mnistTrainLabels[j];
+                distArr[j][0]=Math.sqrt(distArr[j][0]/mnistTrainArray[j].length);
+            }
+            // sorts the array from least distance from the test point to most distance
+            Arrays.sort(distArr, Comparator.comparingDouble(o -> o[0]));
+            guess[i]=neighbors(distArr,knn);
+        }
+        
+    }
+ } 
